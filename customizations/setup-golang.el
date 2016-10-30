97cdc3e6a3702e36b32856a933990d84c6b88d3a
Dmitry Shulyak <dshulyak@mirantis.com>
2016-10-30 12:58:44 +0200

    Emacs configuration with customizations

---
+++ customizations/setup-golang.el
@@ -0,0 +1,13 @@
+(require 'company)
+(require 'flycheck)
+(require 'go-eldoc)
+(require 'company-go)
+
+(add-hook 'before-save-hook 'gofmt-before-save)
+(setq-default gofmt-command "goimports")
+(add-hook 'go-mode-hook 'go-eldoc-setup)
+(add-hook 'go-mode-hook (lambda ()
+                            (set (make-local-variable 'company-backends) '(company-go))
+                            (company-mode)))
+(add-hook 'go-mode-hook 'yas-minor-mode)
+(add-hook 'go-mode-hook 'flycheck-mode)
